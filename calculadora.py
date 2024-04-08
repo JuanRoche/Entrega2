ead4a8902e63130098fb70a60810c38d51dfe629
Juan Andres Roche <rochejuanandres@gmail.com>
2024-04-08 17:06:07 -0300

    Inciso 3

--- calculadora.py
+++ calculadora.py
@@ -13,8 +13,8 @@ def generate_dic(names, goals, goals_avoided, assists):
         dic[names_list[i]] = stats.copy()
     return dic
 
-
-def calcular_goleador(dic):
+def conocer_goleador(dic):
+    """conocer_goleador recorre el diccionario y devuleve en una tupla el nombre y cantidad de goles del goleador"""
     max = -1
     for name in dic:
         # valor del dic = [goles, goles evitados, asistencias]
@@ -22,4 +22,16 @@ def calcular_goleador(dic):
             max_scorer = name
             max = dic[name][0]
     return max_scorer, max
+
+def conocer_influyente(dic,values):
+    """conocer_influyente recorre el diccionario buscando el jugador con mas puntos y devuelve en una tupla su nombre y cantidad de puntos."""
+    max = -1
+    for name in dic:
+        stats = dic[name]
+        points = 0
+        points += stats[0] * values["goals"] + stats[1] * values["goals_avoided"] + stats[2] * values["assists"]
+        if points >= max:
+            most_influential = name
+            max = points
+    return most_influential, max
 
